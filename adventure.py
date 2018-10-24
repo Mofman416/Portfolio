5ead7f954d5b174c35dcb983fb4fd28b7b03eae4
Mofman416 <goodnightolks26@gmail.com>
2018-10-23 18:38:38 -0600

    Added some more code. Gave the Chapel branch.

--- adventure.py
+++ adventure.py
@@ -1,6 +1,7 @@
 #The Great Adventure
 
 import random
+import time
 phealth = 100
 poriginal = 100
 playerdamage = random.randrange(0,30)
@@ -21,7 +22,6 @@ def adventure():
             break
             
         else:
-            break
             print("Invalid response. Try again.")
             print()
 
@@ -46,10 +46,42 @@ def tut(name):
         print(name + "'s battle took them to a new part of town.")
         print()
         print("To your left is a huge building made of wood, and to your right is a large factory.")
-        choice = input("Will", name, "go left (Chapel) or right (Factory)? ")
-        if choice.lower().startswith("c"):
-            chapel()
-        
+        while True:
+            choice = input("Will " + name + " go left (Chapel) or right (Factory)? ")
+            print()
+            if choice.lower().startswith("c"):
+                chapel(name)
+                print("Now " + name + " is outside of the Chapel. They can still go into the factory.")
+                print()
+                while True:
+                    choice = input("Go to the Factory (Factory) or go Home (Home)? ")
+                    if choice.lower().startswith("f"):
+                        factory(name)
+                    elif choice.lower().startswith("h"):
+                        print(name, "goes home. ")
+                        gameover()
+                        break
+                    else:
+                        print("Invalid Response. Try Again.")
+                        print()
+            elif choice.lower().startswith("f"):
+                factory(name)
+                print("Now " + name + " is outside of the Factory. They can still go into the chapel.")
+                print()
+                while True:
+                    choice = input("Go to the Chapel (Chapel) or go Home (Home)? ")
+                    if choice.lower().startswith("c"):
+                        chapel(name)
+                    elif choice.lower().startswith("h"):
+                        print(name, "goes home. ")
+                        gameover()
+                        break
+                    else:
+                        print("Invalid Response. Try Again.")
+            else:
+                print("Invalid Response. Try again.")
+                print()
+            
         
         
     
@@ -80,6 +112,8 @@ def combat(monsterdamage, mhealth, monster, phealth, poriginal, playerdamage, mo
         if mhealth <= 0:
             print("You have defeated", monster, "and your health has been restored!")
             print()
+            print("COMBAT RESOLVED!")
+            print()
             phealth = 100
             break
         elif phealth > 0 and mhealth > 0:
@@ -92,10 +126,15 @@ def combat(monsterdamage, mhealth, monster, phealth, poriginal, playerdamage, mo
                 print()
             elif action.lower().startswith("r"):
                 break
+            else:
+                print("Invalid Response. Try Again.")
     
     if action.lower().startswith("r"):
         print("You run from " + monster + ".")
         print()
+
+    else:
+        print("Invalid option. Try again.")
         
 def gameover():
     answer = input("Do you want to restart (Restart) or Quit (Quit)? ").lower()
@@ -104,10 +143,104 @@ def gameover():
         adventure()
     elif answer.startswith("q"):
         print("Thank you for playing! Have a good day!")
+        time.sleep(10)
     else:
         print("Quit messing around, punk.")
         print()
         gameover()
+        
+def chapel(name):
+    print(name, "walks into the old chapel.")
+    print()
+    print("The floors are creaky and the pews are skewed.")
+    print()
+    print("Suddenly, a bat appears!")
+    print()
+    monster = "Bat"
+    monsterdamage = random.randrange(0,20)
+    mhealth = 20
+    monsterimage = """
+                      -------------    *        *      -------------
+                      \___         \  * *      * *    /         ___/
+                       \____         *%%%*    *%%%*          _____/
+                        \___________*%%%%%****%%%%%*_____________/
+                                    *     . .      *
+                                    *      W       *
+                                    ****************"""
+    combat(monsterdamage, mhealth, monster, phealth, poriginal, playerdamage, monsterimage, name)
+    print(name, "continues inside and sees a cirlce drawn on the ground. On certain points has a lit candle except for one.")
+    print()
+    while True:
+        choice = input("Will you light the last candle (Light) or leave (Home)? ")
+        if choice.lower().startswith("l"):
+            print("The candle is lit.")
+            print()
+            print("The ground shakes, the world darkens...")
+            print()
+            print("A wraith emerges and goes for you!!")
+            print()
+            monster = "Wraith"
+            monsterimage = """
+                                *******
+                                *%%%%%*
+                                *%%%%%*
+                                *******
+                                   *
+                                ******
+                                   *
+                                   *
+                                   *
+                                  * *
+                                 *   *"""
+            monsterdamage = random.randrange(0,20)
+            mhealth = 60
+            combat(monsterdamage, mhealth, monster, phealth, poriginal, playerdamage, monsterimage, name)
+            print("The Wraith disappears and " + name + "finds a bookshelf.")
+            print()
+            print("They begin reading.")
+            print()
+            print("'Our town is gorgeous. We thank the forces that created the land and all that it has provided us with. We use the land as a means for nourishment and giving thanks.'")
+            print()
+            choice = input("Continue reading (Y/N)? ")
+            while True:
+                if choice.upper() == "Y":
+                    print("'The town began to decay. We're not sure what happened but people began dying off at a faster rate than usual.'")
+                    print()
+                    choice = input("Continue reading (Y/N)? ")
+                    print()
+                    if choice.upper() == "Y":
+                        print("'As if things couldn't get worse, the food became poisoned and unfit for consumption. We're running low on options.'")
+                        print()
+                        choice = input("Continue reading (Y/N)? ")
+                        if choice.upper() == "Y":
+                            print("'Things are just horrible. People have resorted to eating each other. It's a truly nauseating thing. I am ashamed to admit that I have fallen to this activity. At first people only began eating the dead. Now they hunt each other like animals.'")
+                            print()
+                            choice = input("Continue reading (Y/N)? ")
+                            if choice.upper() == "Y":
+                                print("'It's awful. The remaining saints and I have decided to summon the forces for their aid. Something very evil came. I can hear my friends being slaughtered as I write this. It won't be long before-'")
+                                print()
+                                print("The record ends there.")
+                                print()
+                                break
+                    break
+                                
+                elif choice.upper() == "N":
+                    print(name, "replaces the book and leaves. That's enough adventuring for one day.")
+                    print()
+                    gameover()
+                    break
+                else:
+                    print("Invalid Response. Try Again.")
+            break
+                
+        elif choice.lower().startswith("l"):
+            print(name, "left the chapel and went home.")
+            print()
+            gameover()
+            break
+        else:
+            print("Invalid Choice. Try Again.")
+
 
 print("Welcome to the Great Adventure!")
 print()

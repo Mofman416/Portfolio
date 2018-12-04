f2a7191a45394677616ee6eca1e0d71557e2926f
Mofman416 <goodnightolks26@gmail.com>
2018-12-03 18:35:44 -0700

    Split the program into functions.

--- hangman.py
+++ hangman.py
@@ -7,187 +7,196 @@ import random
 import time
 
 #constants
-def hangman():
-    HANGMAN = (
-    """
+HANGMAN = (
+"""
 
-    ---------
-    |       |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |       +
-    |       
-    |      
-    |
-    |
-    |
-    |
-    |  
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |      -+
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |      -+-
-    |       
-    |       
-    |
-    |
-    |
-    |
-    |  
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |      -+-
-    |       |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |      -+-
-    |       |
-    |      / 
-    |
-    |
-    |
-    |
-    |  
-    |
-    |
-    __________
-    """,
-    """
-    ---------
-    |       |
-    |       O
-    |      -+-
-    |       |
-    |      / \
-    |
-    |
-    |
-    |
-    |  DEAD
-    |
-    |
-    __________
-    """)
+---------
+|       |
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|
+|
+|
+|
+|
+|
+|
+|
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|       +
+|       
+|      
+|
+|
+|
+|
+|  
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|      -+
+|
+|
+|
+|
+|
+|
+|
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|      -+-
+|       
+|       
+|
+|
+|
+|
+|  
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|      -+-
+|       |
+|
+|
+|
+|
+|
+|
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|      -+-
+|       |
+|      / 
+|
+|
+|
+|
+|  
+|
+|
+__________
+""",
+"""
+---------
+|       |
+|       O
+|      -+-
+|       |
+|      / \
+|
+|
+|
+|
+|  DEAD
+|
+|
+__________
+""")
 
-    MAX_WRONG=len(HANGMAN)-1
-    WORDS= ("OVERUSED", "CALM", "GUAM", "TAFFETA", "PYTHON")
-    word = random.choice(WORDS) #The word the player will need to guess.
-    so_far = "_"*len(word)
-    wrong = 0 #number of wrong guesses the player made
-    used = [] #The incorrect letters used.
+MAX_WRONG=len(HANGMAN)-1
+WORDS= ("OVERUSED", "CALM", "GUAM", "TAFFETA", "PYTHON")
+word = random.choice(WORDS) #The word the player will need to guess.
+so_far = "_"*len(word)
+wrong = 0 #number of wrong guesses the player made
+used = [] #The incorrect letters used.
 
-def game():
-    print("Welcome to Hangman. Have fun!")
+#Game Over
+def game_over(wrong, HANGMAN):
+    print(HANGMAN[wrong])
+    print("\nYou've been hanged!")
 
-    while wrong <MAX_WRONG and so_far != word:
-        hangman()
-        print(HANGMAN[wrong])
-        print("\nYou've used the following letters:\n", used)
-        print("\nSo far, the word is:\n", so_far)
+#This function shows up if you guess a letter already used.
+def use(guess):
+    print("You've already guessed the letter", guess)
+    guess = input("Enter your guess: ")
+    guess = guess.upper()
 
-        guess = input("\n\nEnter your guess: ")
-        guess = guess.upper()
-        
-        while guess in used:
-            print("You've already guessed the letter", guess)
-            guess = input("Enter your guess: ")
-            guess = guess.upper()
-
-        used.append(guess)
-
-        if guess in word:
-            print("\nYes!", guess, "is in the word.")
-
-            # create a new so_far to include guess
-            new = ""
-            for i in range(len(word)):
-                if guess == word[i]:
-                    new += guess
-                else:
-                    new += so_far[i]
-            so_far = new
-        else:
-            print("\nSorry,", guess, "isn't in the word.")
-            wrong += 1
-
-    if wrong == MAX_WRONG:
-        print(HANGMAN[wrong])
-        print("\nYou've been hanged!")
-
-    else:
-        print("\nYou guessed it!")
+#This function is called when the player guessed the word.
+def win(word):
+    print("\nYou guessed it!")
     print("\nThe word was", word)
 
     input("\n\nPress the enter key to exit.")
+    
 
-game()
+print("Welcome to Hangman. Have fun!")
+
+while wrong <MAX_WRONG and so_far != word:
+    print(HANGMAN[wrong])
+    print("\nYou've used the following letters:\n", used)
+    print("\nSo far, the word is:\n", so_far)
+
+    guess = input("\n\nEnter your guess: ")
+    guess = guess.upper()
+    
+    while guess in used:
+        use(guess)
+
+    used.append(guess)
+
+    if guess in word:
+        print("\nYes!", guess, "is in the word.")
+
+        # create a new so_far to include guess
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+    else:
+        print("\nSorry,", guess, "isn't in the word.")
+        wrong += 1
+
+if wrong == MAX_WRONG:
+    game_over(wrong, HANGMAN)
+
+else:
+    win(word)
+

7fb41368b2ed6fb9bdc0a85740cb8937e526db83
Michael Freeman <michael.freeman@tooeleschools.org>
2018-12-12 13:08:33 -0700

    Continued making the program with the class.

--- TTT/tic tac toe.py
+++ TTT/tic tac toe.py
@@ -7,7 +7,7 @@ X = "X"
 O = "O"
 EMPTY = " "
 TIE = "TIE"
-NUM_SQUARE = 9
+NUM_SQUARES = 9
 
 #functions
 def instructions():
@@ -37,7 +37,7 @@ def ask_permission(question):
     """Ask a yes or no question."""
     response = None
     while response not in ("y", "n"):
-        response = input(question+ "y or n").lower()
+        response = input(question+ "Y or N ").lower()
     return response
 
 def ask_num(question, low, high):
@@ -47,18 +47,67 @@ def ask_num(question, low, high):
         if response.isdigit():
             if int(response) in range(low, high):
                 break
-                break
             elif response not in range(low, high):
                 response = input(question)
         else:
             print("Invalid, try again")
             response = input(question)
     return int(response)
+
+def pieces():
+    #Here we assign who goes first and assign which one goes to X or O
+    go_first = ask_permission("Do you want to go first? ")
+    if go_first == "y":
+        print("I think I'll let you get a head start.\n")
+        human = X
+        computer = O
+    else:
+        print("You must be very brave or quite incompetent!\n")
+        human = O
+        computer = X
+    return human, computer
+
+def new_board():
+    #This is the new board to make.
+    board = []
+    for squares in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
+
+def display_board(board):
+    #This disaplys the board on the screen.
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\n\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\n\t", board[6], "|", board[7], "|", board[8])
+
+def legal_moves(board):
+    #Creates a list of legal moves
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
+
+def winner(board):
+    #This function defines winning conditions
+    WAYS_TO_WIN = ((0,1,2),
+            (3,4,5),
+            (6,7,8),
+            (0,3,6),
+            (1,4,7),
+            (2,5,8),
+            (0,4,8),
+            (2,4,6))
+    for row in WAYS_TO_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+            winner = board[row[0]]
+            return winner
+
+        if EMPTY in board:
+            return TIE
+        
+        return None
         
 
-#instructions()
-
-#ask_permission("Do you want something to eat?")
-
-x=ask_num("Input a number between 0 and 8 ",0, 9)
-print(x)

89ec63de5da69612adb0f678ba840a024352574d
Michael Freeman <michael.freeman@tooeleschools.org>
2019-01-04 14:15:37 -0700

    Fnished the rough code. Now I just need to complete the text file!

--- Trivia/trivia.py
+++ Trivia/trivia.py
@@ -18,9 +18,64 @@ def read_line(file):
     line = line.replace("/", "\n")
     return line
 
-file = open_file("test_file.txt", "w")
-file.write("this/is/a/Test")
-file.close()
-file = open_file("test_file.txt", "r")
-line = read_line(file)
-print(line)
+##file = open_file("test_file.txt", "w")
+##file.write("this/is/a/Test")
+##file.close()
+##file = open_file("test_file.txt", "r")
+##line = read_line(file)
+##print(line)
+################################################################################
+def next_block(file):
+    #This returns a block of data from the trivia file
+    category = read_line(file)
+    question = read_line(file)
+    answers = []
+    for i in range(4):
+        answer = read_line(file)
+        answers.append(answer)
+    correct = read_line(file)
+    if correct:
+        correct = correct[0]
+    explanation = read_line(file)
+    return category, question, answers, correct, explanation
+################################################################################
+def welcome(title):
+    #Welcome the player and get his/her name
+    print("\t\tWelcome to Trivia Challenge!\n")
+    print("\t\t", title, "\n")
+################################################################################
+def main():
+    file = open_file("test_file.txt", "r")
+    title = read_line(file)
+    welcome(title)
+    score = 0
+    category, question, answers, correct, explanation = next_block(file)
+    while category:
+        print(category)
+        print(question)
+        for i in range(4):
+            print(i+1, answers[i])
+        print(correct)
+        answer = input("Put in 4. ")
+        if answer == correct:
+            print("Cool.")
+            score += 1
+        else:
+            print("No.")
+        print(explanation)
+        print(score)
+        category, question, answers, correct, explanation = next_block(file)
+    file.close()
+    print("You've completed the quiz!")
+    return score
+
+##file = open_file("test_file.txt", "r")
+##category, question, answers, correct, explanation = next_block(file)
+##print(category)
+##print(question)
+##print(answers)
+##print(correct)
+##print(explanation)
+
+show = main()
+print(show)

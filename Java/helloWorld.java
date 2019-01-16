f14089d212c3a6e9c458a9204578fde483505a66
Michael Freeman <michael.freeman@tooeleschools.org>
2019-01-16 09:54:02 -0700

    Moved programs depending on what code they were written in. Completed humanclass, started Java

---
+++ Java/helloWorld.java
@@ -0,0 +1,15 @@
+// Michael Freeman
+// 1/19
+/*
+This is my first java program
+*/
+
+class HelloWord{
+
+	//main entry point to program
+	public static void main(String[] args){
+		// programing statement
+		System.out.println("hello, World");
+	}
+
+}
--- Python/humanclass.py
+++ Python/humanclass.py
@@ -1,3 +1,4 @@
+#This is an example of classes
 class Human(object):
 
     def __init__(self,name,hair_color,eye_color,height,weight,iq,gender,race):
@@ -14,7 +15,7 @@ class Human(object):
     def describe_self(self):
         print("I have "+self.hair_color+ " hair")
         print("I have "+self.eye_color+" eyes")
-        print("I am",self.height,"tall")
+        print("I am",self.height,"inches tall")
         print("I weigh",self.weight,"lbs")
         print("I am a "+self.race+" "+self.gender+" with an iq of ",self.iq)
     def learn(self):
@@ -24,16 +25,38 @@ class Human(object):
             if int(answer)==4:
                 print("You go it!")
                 self.iq += 1
-                print(self.iq)
+                print("Your IQ is now", self.iq)
             else:
                 print("Wrong!")
         else:
             print("Invalid input!")
+    def bmi(self):
+        print("Let's find out what your BMI is given you weight of", self.weight, "and your height of", self.height, "\n")
+        num1 = self.weight*0.45
+        num2 = self.height*0.025
+        num3 = num2*num2
+        bmi = num1//num3
+        print("With you weight and height put through functions and math, you BMI is", bmi, "\n")
+        if bmi >= 12 and bmi <=18:
+            print("You are underweight!")
+        elif bmi >=18 and bmi <=24:
+            print("You are at a healthy weight for your height!")
+        elif bmi >= 25 and bmi <= 29:
+            print("You are overweight!")
+        elif bmi >= 30 and bmi <= 39:
+            print("You are obese!")
+        elif bmi >= 40 and bmi <= 42:
+            print("You are extremely obese!")
+        else:
+            print("Sorry, I can't seem to calculate your BMI. This could be due to either too low or too high BMI.")
+
+
             
             
-mike = Human("Mike", "Brown", "Hazel", 6, 151, 90, "Male", "White")
+mike = Human("Mike", "Brown", "Hazel", 72, 151, 90, "Male", "White")
 mike.intro_self()
 mike.describe_self()
 mike.learn()
+mike.bmi()
 
 #Make a function to calculare the BMI of an individual. Compare it to a BMI chart

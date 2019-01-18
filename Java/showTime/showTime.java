9abfbc140f84fe58651fb5e117a24d290edf0400
Michael Freeman <michael.freeman@tooeleschools.org>
2019-01-18 10:10:58 -0700

    More experimentation with Java coding, added a place for unfinished code.

---
+++ Java/showTime/showTime.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+/*
+Mofman416
+1/19
+show current time in console
+*/
+
+public class showTime{//start class
+	public static void main(String[] args){
+		System.out.println(Calendar.getInstance().getTime());
+	
+	}
+
+}//end class

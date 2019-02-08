2ad6bdaa600dbdc86453575c0ef1378a3ac120bb
Michael Freeman <michael.freeman@tooeleschools.org>
2019-02-08 10:16:14 -0700

    Larning how to use different layouts!

--- Unfinished Code/Java/Mobile App/ch12 examples/src/ch12/SimpleGUI.java
+++ Unfinished Code/Java/Mobile App/ch12 examples/src/ch12/SimpleGUI.java
@@ -1,40 +1,44 @@
 package ch12;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class SimpleGUI {
+public class SimpleGUI implements ActionListener{
+	JFrame myFrame;
+	JButton myButton1;
+	JButton myButton2;
 	
 	public static void main(String[] args) {
-		//object creation
-		JFrame myFrame = new JFrame();
-		FlowLayout myLayout = new FlowLayout();
-		JLabel myLabel = new JLabel("This is my label!");
-		JButton myButton = new JButton("Click Here");
-		
-		
-		//setup
-		myButton.setText("Don't click me.");
-		myLabel.setText("This is my label!");
-		myFrame.setLayout(myLayout);
-	    myFrame.setTitle("JFrame Window");
-	    myFrame.setSize(400, 200);
-	    myFrame.setLocation(200, 300);
-	    myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
-	    //draw / render
-	    myFrame.add(myLabel);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    myFrame.add(myButton);
-	    
-	    myFrame.pack();
-	    
-	    myFrame.setVisible(true);
+		new SimpleGUI();
+	}
+
+	public SimpleGUI() {
+	   myFrame = new JFrame();        // create the JFrame window
+	   myFrame.setLayout(new FlowLayout());  // attach manager 
+	   myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	   myButton1 = new JButton("Button 1"); 
+	   myButton1.addActionListener(this);
+	   myFrame.add(myButton1);     // add JButton to JFrame
+	   myButton2 = new JButton("Button 2"); 
+	   myButton2.addActionListener(this);
+	   myFrame.add(myButton2);
+
+	   myFrame.pack();
+	   myFrame.setVisible(true); 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		Object control = event.getSource();
+		   if (control == myButton1)  // if myButton1 was clicked
+		   {
+		      JOptionPane.showMessageDialog(myFrame,"I like Red!");
+		   }
+		   else if (control == myButton2)  // else if myButton2 was clicked
+		   {
+		      JOptionPane.showMessageDialog(myFrame,"I like Blue!");
+		   }
 		
 	}
 

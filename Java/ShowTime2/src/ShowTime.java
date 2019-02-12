f444b8b48dd865861eb73cde291f4bd0465bd877
Mofman416 <goodnightolks26@gmail.com>
2019-02-12 12:39:59 -0700

    ?

---
+++ Java/ShowTime2/src/ShowTime.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+
+public class ShowTime{//start class
+	public static void main(String[] args){
+		System.out.println(Calendar.getInstance().getTime());
+	
+	}
+
+}//end class

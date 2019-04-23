f4e6a0cc06de0f8ef2b0a132fbc19a506b47116d
Mofman416 <goodnightolks26@gmail.com>
2019-04-22 22:38:03 -0600

    Began more work on Pygame examples.

--- Unfinished Code/Python/PyGameTemplate/sprite example.py
+++ Unfinished Code/Python/PyGameTemplate/sprite example.py
@@ -14,6 +14,22 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+class Player(pygame.sprite.Sprite):
+
+    # Sprite for the player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
 # Set up the screen.
 
 # Initializes the game.
@@ -26,6 +42,8 @@ pygame.display.set_caption("My Game.")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game loop
 running = True
 while running:

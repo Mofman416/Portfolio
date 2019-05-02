a1b628ce9d34c6d570aa7fea8afade893cd74662
Mofman416 <goodnightolks26@gmail.com>
2019-05-01 22:14:51 -0600

    Began work on my Final Project titled 'NiceDay', more work to the platformer example.

--- Unfinished Code/Python/platformer/settings.py
+++ Unfinished Code/Python/platformer/settings.py
@@ -9,6 +9,14 @@ TITLE = "Jumpy"
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
 
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20),
+                 ]
+
 # Define Colors.
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)

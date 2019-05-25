16ae8f937d7e0399c745cdf8679cbe77ce6fcad3
Mofman416 <mofman1@hotmail.com>
2019-05-25 17:48:20 -0600

    More work to Turf War, now if run it will produce a black screen.

--- Unfinished Code/Python/turfwar/main.py
+++ Unfinished Code/Python/turfwar/main.py
@@ -5,4 +5,36 @@ import pygame as py
 from sprites import *
 from settings import *
 
+# Set up the screen.
 
+# Initializes the game.
+pg.init()
+
+# Initializes sound.
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My Game.")
+clock = pg.time.Clock()
+
+all_sprites = pg.sprite.Group()
+# Game loop
+running = True
+while running:
+    # Keep loop running at the right speed.
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pg.event.get():
+        # Check for closing the window.
+        if event.type == pg.QUIT:
+            running = False
+
+    # Update
+    all_sprites.update()
+
+    # Draw / Render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display.
+    pg.display.flip()
+
+pg.quit()

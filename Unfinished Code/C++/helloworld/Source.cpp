375b2c43f140bb8589aa9f79bbefa8b7ae83c6fe
Mofman416 <mofman1@hotmail.com>
2019-05-26 18:49:55 -0600

    Did my Hello World! program in C++. I also got a new demo file in the completed code area for C++.

---
+++ Unfinished Code/C++/helloworld/Source.cpp
@@ -0,0 +1,17 @@
+// Simple C++ program to display "Hello World!"
+
+// Header file for input/output function
+#include<iostream>
+
+using namespace std;
+
+// Main function
+// Where the execution of the file begins.
+
+int main()
+{
+	// Prints Hello World
+	cout << "Hello World!";
+
+	return 0;
+}

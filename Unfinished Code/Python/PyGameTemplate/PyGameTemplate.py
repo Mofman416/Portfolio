d491ff112d2330de55c3681f5e7569da307ae974
Mofman416 <mofman1@hotmail.com>
2019-06-02 19:43:02 -0600

    Setup eclipse, began work on the Tilegame from Kids Can Code.

--- Unfinished Code/Python/PyGameTemplate/PyGameTemplate.py
+++ Unfinished Code/Python/PyGameTemplate/PyGameTemplate.py
@@ -1,10 +1,19 @@
-   # PyGame template
+# PyGame template
 import pygame
 import random
 
 
-
 # Set up the screen.
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# Set up colors.
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 225)
 
 # Initializes the game.
 pygame.init()

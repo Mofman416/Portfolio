ab569e715e587946bfff852209552208e627c20a
Mofman416 <mofman1@hotmail.com>
2020-02-04 22:21:54 -0500

    Added some more properties to the RadioButtons. TODO: Fix RadioButton behavior.

--- Unfinished Code/Python/pizza/pizza.py
+++ Unfinished Code/Python/pizza/pizza.py
@@ -9,6 +9,9 @@ class Pizza(Frame):
         super(Pizza, self).__init__(master)
         self.grid()  # This is absolutely vital for future GUI projects!
         self.create_widgets()
+        self.name = ""
+        self.size = StringVar
+        self.size.set(self, value="Small")
 
     def create_widgets(self):
         """This creates the input types"""
@@ -16,20 +19,20 @@ class Pizza(Frame):
         self.name_label = Label(self, text="Name: ")
         self.name_label.grid(row=0, column=0)
 
-        self.entername = Text(self, width=35, height=5)
+        self.entername = Text(self, width=35, height=1)
         self.entername.grid(row=0, column=1, columnspan=3)
 
         # Adds Radiobuttons for users to interact with
         self.size_label = Label(self, text="Size:")
         self.size_label.grid(row=1, column=0)
 
-        self.size_sml = Radiobutton(self, text="Small")
+        self.size_sml = Radiobutton(self, variable=self.size, value="Small", text="Small")
         self.size_sml.grid(row=1, column=1)
 
-        self.size_med = Radiobutton(self, text="Medium")
+        self.size_med = Radiobutton(self, variable=self.size, value="Medium", text="Medium")
         self.size_med.grid(row=1, column=2)
 
-        self.size_lrg = Radiobutton(self, text="Large")
+        self.size_lrg = Radiobutton(self, variable=self.size, value="Large", text="Large")
         self.size_lrg.grid(row=1, column=3)
 
 

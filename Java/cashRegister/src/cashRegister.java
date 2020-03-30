b37a1f01dac62e2b298f5871d4eead9322660b59
Mofman416 <mofman1@hotmail.com>
2020-03-30 16:36:37 -0400

    Created a cashRegister program.

---
+++ Java/cashRegister/src/cashRegister.java
@@ -0,0 +1,29 @@
+// Michael Freeman
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.*;
+
+public class cashRegister implements ActionListener {
+    JFrame cashFrame;
+
+
+    public static void main(String[] args) {
+        new cashRegister();
+
+    }
+
+    public cashRegister() {
+        // This is the frame for the register
+        JFrame cashFrame = new JFrame(title: "Cash Register");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}

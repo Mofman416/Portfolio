fe27c6fded4c1a8d1a3806613227f0734d3ae405
Mofman416 <mofman1@hotmail.com>
2020-04-01 17:13:04 -0400

    More work to the Cash Register program.

--- Java/cashRegister/src/cashRegister.java
+++ Java/cashRegister/src/cashRegister.java
@@ -9,6 +9,8 @@ import javax.swing.*;
 
 public class cashRegister implements ActionListener {
     JFrame cashFrame;
+    JPanel mainPanel;
+    JTextField cname;
 
 
     public static void main(String[] args) {
@@ -18,7 +20,22 @@ public class cashRegister implements ActionListener {
 
     public cashRegister() {
         // This is the frame for the register
-        JFrame cashFrame = new JFrame(title: "Cash Register");
+        JFrame cashFrame = new JFrame("Cash Register");
+        cashFrame.setLayout(new FlowLayout());
+        cashFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = (JPanel)cashFrame.getContentPane();
+        mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        // Text field test
+        //Text field asking for the customer name.
+        JPanel tfield = new JPanel();
+        JLabel name1 = new JLabel("Customer Name:");
+        tfield.add(name1);
+        cname = new JTextField(20);
+        tfield.add(cname);
+        mainPanel.add(tfield);
 
     }
 
